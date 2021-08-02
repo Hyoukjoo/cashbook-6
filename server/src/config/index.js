83fa545660aaec2590032a94973bd13a1a95aecb
momongss <rkrp15@gmail.com>
2021-08-03 00:59:56 +0900

    🔧 config 파일 추가

---
+++ server/src/config/index.js
@@ -0,0 +1,14 @@
+import * as dotenv from "dotenv";
+
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
+if (process.env.NODE_ENV !== "production") {
+  const envFound = dotenv.config();
+  if (envFound.error) throw new Error("Couldn't find .env file");
+}
+
+export default {
+  port: parseInt(process.env.PORT, 3000),
+  github_client_id: process.env.GITHUB_CLIENT_ID,
+  github_client_secret: process.env.GITHUB_CLIENT_SECRET,
+};
