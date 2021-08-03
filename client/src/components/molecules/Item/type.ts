4b789554f0fa09644e7352a8fa2be6cda30071d8
HJ <jo920208@gmail.com>
2021-08-03 19:43:05 +0900

    💄 Molecule > LargeHistoryItem, SmallHistoryItem

---
+++ client/src/components/molecules/Item/type.ts
@@ -0,0 +1,5 @@
+import { CategoryHistory, History } from "apis/history/dto";
+
+export interface LargeHistoryItemProps extends History {}
+
+export interface SmallHistoryItemProps extends CategoryHistory {}
