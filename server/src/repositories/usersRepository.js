1940fcca7b6c4ff923145235bad2779c25e068b5
momongss <rkrp15@gmail.com>
2021-08-03 14:39:23 +0900

    ✨ oauth 로그인, 회원가입 기능 구현

--- server/src/repositories/usersRepository.js
+++ server/src/repositories/usersRepository.js
@@ -9,4 +9,59 @@ export const usersRepository = {
 
     return user;
   },
+
+  signup: async (email, github_oauth_id) => {
+    const connection = database.connection.getRepository(Users);
+    const savedUser = await connection.save({
+      email,
+      github_oauth_id,
+      github_oauth_token: "",
+    });
+  },
+
+  updateGithubOauthToken: async (github_oauth_id, github_oauth_token) => {
+    const connection = database.connection.getRepository(Users);
+    const user = await connection.findOne({
+      where: { github_oauth_id, github_oauth_id },
+    });
+
+    user.github_oauth_token = github_oauth_token;
+    const savedUser = await connection.save(user);
+  },
+
+  getUser: async (id) => {
+    const connection = database.connection.getRepository(Users);
+    return await connection.findOne({ where: { id: id } });
+  },
+
+  getUserByGithubId: async (github_oauth_id) => {
+    const connection = database.connection.getRepository(Users);
+    return await connection.findOne({
+      where: { github_oauth_id: github_oauth_id },
+    });
+  },
+
+  deleteUserByEmail: async (email) => {
+    const connection = database.connection.getRepository(Users);
+    const deleteResult = await connection.delete({ email: email });
+
+    if (deleteResult.affected == null || deleteResult.affected === 0) {
+      return false;
+    } else {
+      return true;
+    }
+  },
+
+  deleteUserByGithubId: async (github_oauth_id) => {
+    const connection = database.connection.getRepository(Users);
+    const deleteResult = await connection.delete({
+      github_oauth_id: github_oauth_id,
+    });
+
+    if (deleteResult.affected == null || deleteResult.affected === 0) {
+      return false;
+    } else {
+      return true;
+    }
+  },
 };
