ebec8c7e216dddeb0d5cf2a756eb2e02a0030409
HJ <jo920208@gmail.com>
2021-08-04 18:35:18 +0900

    🏷️ Molecule > Container: type 제거

--- client/src/components/molecules/Container/index.ts
+++ client/src/components/molecules/Container/index.ts
@@ -3,7 +3,7 @@ import "./style.scss";
 import { main, section } from "atoms/Base";
 import { Molecule } from "molecules/type";
 
-const Container: Molecule<HTMLElement> = (...$element) => {
+const Container = (...$element) => {
   const $main = main();
   const $section = section();
 
