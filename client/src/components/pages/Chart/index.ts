afab02e91b569e112463f43425a2eeec050f0a36
momongss <rkrp15@gmail.com>
2021-08-04 21:19:34 +0900

    ✨도넛 그래프 구현 ( 애니메이션 X )

--- client/src/components/pages/Chart/index.ts
+++ client/src/components/pages/Chart/index.ts
@@ -2,6 +2,8 @@ import { HistoryType } from "apis/history/dto";
 import Container from "molecules/Container";
 import CheckBoxInput from "molecules/Input/CheckBox";
 
+import Canvas from "../../canvas/index";
+
 // import LargeHistoryList from "molecules/List/LargeHistoryList";
 import SmallHistoryList from "molecules/List/SmallHistoryList";
 import { Page } from "pages/type";
@@ -34,38 +36,48 @@ const Chart: Page = (targetElement, state) => {
   //   ],
   // });
 
+  const categoryHistories = [
+    {
+      name: "생활",
+      color: "#4A6CC3",
+      rate: 58,
+      total: 301000,
+    },
+    {
+      name: "식비",
+      color: "#4CA1DE",
+      rate: 28,
+      total: 201000,
+    },
+    {
+      name: "교통",
+      color: "#94D3CC",
+      rate: 10,
+      total: 51000,
+    },
+    {
+      name: "문화/여가",
+      color: "#D092E2",
+      rate: 10,
+      total: 31000,
+    }
+  ];
+
+  const $canvasWrapper = document.createElement("div");
+  $canvasWrapper.className = "canvas-wrapper";
+
+  const canvas = new Canvas();
+  console.log(canvas.$canvas);
+
+  $canvasWrapper.appendChild(canvas.$canvas);
+
   const $list = SmallHistoryList({
-    categoryHistories: [
-      {
-        name: "생활",
-        color: "#21a5b1",
-        rate: 38,
-        total: 301000,
-      },
-      {
-        name: "술",
-        color: "#2145b1",
-        rate: 28,
-        total: 201000,
-      },
-      {
-        name: "술",
-        color: "#2145b1",
-        rate: 28,
-        total: 201000,
-      },
-      {
-        name: "술",
-        color: "#2145b1",
-        rate: 28,
-        total: 201000,
-      },
-    ],
+    categoryHistories: categoryHistories
   });
 
   const $checkbox = CheckBoxInput({ id: "c1" });
 
-  const $container = Container($checkbox, $list);
+  const $container = Container($checkbox, $list, $canvasWrapper);
 
   $target.append($header, $container);
 
